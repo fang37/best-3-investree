003bdc71edb6e0ccb8ad07effe9cd3c275067588
fang37 <mirfan.ghifari@gmail.com>
2022-06-17 00:44:20 +0700

    [Challenge 2] Refactor elevator class

--- Challenge 2/elevator/elevator/src/Elevator.java
+++ Challenge 2/elevator/elevator/src/Elevator.java
@@ -18,24 +18,28 @@ public class Elevator {
             int inputFloor= scan.nextInt();
 
             if (inputFloor >= minFloor && inputFloor <= maxFloor) {
-                while(currentFloor != inputFloor) {
-                    System.out.println("Elevator at floor "+(currentFloor));
-                    if (currentFloor-inputFloor < 0) {
-                        currentFloor++;
-                    }
-                    else if(currentFloor-inputFloor > 0) {
-                        currentFloor--;
-                    }
-                }
-                System.out.println("Elevator stopped at "+currentFloor);
+                moveTo(inputFloor);
             }
             else if (inputFloor == -1) {
-                System.exit(1);
+                return;
             }
             else {
                 System.out.println("Please enter the between " + minFloor +"-"+maxFloor);
             }
 
         }
+    }
+
+    private void moveTo(int inputFloor) {
+        while(currentFloor != inputFloor) {
+            System.out.println("Elevator at floor "+(currentFloor));
+            if (currentFloor-inputFloor < 0) {
+                currentFloor++;
+            }
+            else if(currentFloor-inputFloor > 0) {
+                currentFloor--;
+            }
+        }
+        System.out.println("Elevator stopped at "+currentFloor);
     }
 }
